704c91520cd90d17697d4712fe32a851f897c436
Thomas-George-T <tgt555@gmail.com>
2018-05-18 23:55:06 +0530

    Changing the datalake idea into a more efficient one

--- Spark DataFrames/sparkdatalake/make_datalake.scala
+++ Spark DataFrames/sparkdatalake/make_datalake.scala
@@ -1,7 +1,13 @@
-spark.sql("drop database if exists sparkdatalake cascade")
+// 2nd method is to read the file directly into a dataFrame and create a temp view
+spark.read.textFile("../../Movielens/movies.dat").createOrReplaceTempView("movies_staging");
+spark.read.textFile("../../Movielens/ratings.dat").createOrReplaceTempView("ratings_staging");
+spark.read.textFile("../../Movielens/users.dat").createOrReplaceTempView("users_staging");
+// Make appropriate schemas for them
+spark.sql("drop database if exists cascade sparkdatalake")
 spark.sql("create database sparkdatalake");
-// 2nd method is to read the file directly into a dataFrame
-spark.read.textFile("../../Movielens/movies.dat").write.mode("overwrite").saveAsTable("sparkdatalake.movies_ip");
-spark.read.textFile("../../Movielens/ratings.dat").write.mode("overwrite").saveAsTable("sparkdatalake.ratings_ip");
-spark.read.textFile("../../Movielens/users.dat").write.mode("overwrite").saveAsTable("sparkdatalake.users_ip");
+
+spark.read.textFile("../../Movielens/movies.dat").write.mode("overwrite").saveAsTable("sparkdatalake.movies_staging");
+spark.read.textFile("../../Movielens/ratings.dat").write.mode("overwrite").saveAsTable("sparkdatalake.ratings_staging");
+spark.read.textFile("../../Movielens/users.dat").write.mode("overwrite").saveAsTable("sparkdatalake.users_staging");
+
 System.exit(0)
